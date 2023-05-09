4f7338d1052f0b158b4fdcc5337be237eeb4e0c8
Otsu01 <kudo.nonoka02@gmail.com>
2023-05-09 15:27:21 +0900

    ロジックを使った生成

---
+++ Assets/Scripts/CubeGeneration.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CubeGeneration : MonoBehaviour
+{
+    [SerializeField, Header("CubePrefab")] GameObject _cubePrefab;
+    [SerializeField, Header("Cube’u‚­ŠÔŠu")] float _distance;
+    [SerializeField, Header("¶¬‚·‚éêŠ‚Ì”ÍˆÍ")] Transform[] _startEnd;
+    void Start()
+    {
+        Vector3 generationPos = _startEnd[0].position;
+        Vector3 end = _startEnd[1].position;
+        while (generationPos.z < end.z)
+        {
+            Instantiate(_cubePrefab, generationPos, Quaternion.identity);
+            generationPos.z += _distance;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
